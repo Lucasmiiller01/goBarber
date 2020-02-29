7babc66e84f91d0c0af52e1ecce98f2e6542aa58
Lucas Miiller <lcorrea@unicadtec.com.br>
2020-02-29 20:12:14 -0300

    Criando providercontroller e rota para servir arquivos

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+
 import authMiddleware from './app/middlewares/auth';
 import multer from 'multer';
 import multerConfig from './config/multer';
@@ -16,6 +18,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.post('/users', UserController.store);
+
+routes.get('/providers', ProviderController.index);
+
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
