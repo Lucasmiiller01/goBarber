98b6ada8b0b1ae42143a59ddf110a63f6e2d8e7f
Lucas Miiller <lcorrea@unicadtec.com.br>
2020-03-01 10:05:20 -0300

    Validacoes de agendamento

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,68 @@
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+import { startOfHour, startOfDay, parseISO, isBefore } from 'date-fns';
+import * as Yup from 'yup';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      date: Yup.date().required(),
+      provider_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    /**
+     * Check is provider
+     */
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create appointments with providers' });
+    }
+
+    /**
+     * Check for past dates
+     */
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    /**
+     * Check date availability
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
+    const appointment = await Appointment.create({
+      provider_id,
+      date: hourStart,
+      user_id: req.userId,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
